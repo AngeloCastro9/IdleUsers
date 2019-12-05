01663d701c5c88cb0331f4e756939f824e2eff2b
Ângelo Castro <42939130+AngeloCastro9@users.noreply.github.com>
2019-12-05 11:12:37 -0300

    Version 1.0.1

--- Lithnet.IdleLogoff/LogoffWarning.Designer.cs
+++ Lithnet.IdleLogoff/LogoffWarning.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogoffWarning));
             this.lbWarning = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -38,23 +37,12 @@
             // lbWarning
             // 
             this.lbWarning.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbWarning.Location = new System.Drawing.Point(84, 27);
+            this.lbWarning.Location = new System.Drawing.Point(84, 45);
             this.lbWarning.Name = "lbWarning";
             this.lbWarning.Size = new System.Drawing.Size(431, 71);
             this.lbWarning.TabIndex = 0;
             this.lbWarning.Text = "Your session has been idle for too long, and you will be logged out in {0} second" +
     "s\r\n";
-            // 
-            // button1
-            // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(214, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Cancelar Encerramento";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -67,13 +55,10 @@
             // 
             // LogoffWarning
             // 
-            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.button1;
             this.ClientSize = new System.Drawing.Size(527, 163);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbWarning);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -93,7 +78,6 @@
         #endregion
 
         private System.Windows.Forms.Label lbWarning;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
